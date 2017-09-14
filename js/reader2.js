0529839224ec5b76a6297a07041848e779702b78
Unknown <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-09-14 17:54:12 +0200

    Small changes

--- js/reader2.js
+++ js/reader2.js
@@ -11,3 +11,43 @@ function hideCharacter() {
 function changeCharacter(character) {
 	return document.getElementById("character").textContent = character.name;
 }
+
+function importSave(file) {
+	var levelToLoad = JSON.parse(file);
+	var checksum = levelToLoad.hash;
+	delete levelToLoad.hash;
+	if(checksum === hashCode(JSON.stringify(levelToLoad))) {
+		//console.log(document.getElementById(levelToLoad.levelid).onclick);
+		(document.getElementById(levelToLoad.levelid).onclick)(false);
+		//document.getElementById(levelToLoad.levelid).click(false);
+		levelToLoad.trace.forEach(function(obsel) {
+			var button = document.getElementById(obsel.group);
+			button.click();
+		});
+	} else {
+		window.alert(translate("save_notValid"));
+	}
+}
+
+
+function loadFile() {
+	var fileinput = document.getElementById("fileinput");
+	if(fileinput.files[0] != undefined) {
+		var file = fileinput.files[0];
+
+		if (file.type.match('application/json')) {
+			var reader = new FileReader();
+
+			reader.onload = function(){
+				importSave(reader.result);
+			};
+			reader.readAsText(file);
+    	} else {
+			window.alert(translate("save_wrongFormat"));
+		}
+	}
+}
+
+function readType2() {
+
+}
